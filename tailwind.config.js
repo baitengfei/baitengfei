c56ec2deb8d4075fb425eb41279e4e6400daad38
baitengfei <iterrybai@gmail.com>
2024-10-15 23:30:21 +0800

    header css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}", // 确保包括你的文件路径
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
