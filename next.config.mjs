70672d9febc6736ddeb10a4894a1f89c0335a2f6
baitengfei <iterrybai@gmail.com>
2024-10-17 15:48:10 +0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   /* config options here */
 }
  
-module.exports = nextConfig
+export default nextConfig
