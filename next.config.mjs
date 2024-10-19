e047de2554dc53456265a5c6e48516162c2714b4
baitengfei <iterrybai@gmail.com>
2024-10-19 21:12:16 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,17 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
-}
- 
-export default nextConfig
+  // 保留现有配置
+  async rewrites() {
+      return [
+          {
+              source: '/baidu_verify_codeva-E9J6bjSG7K.html', // 访问的 URL
+              destination: '/baidu_verify_codeva-E9J6bjSG7K', // 实际的 Next.js 页面
+          },
+          // 你可以在这里添加更多的重写规则
+      ];
+  },
+  // 其他配置可以继续保留在这里
+};
+
+export default nextConfig;
